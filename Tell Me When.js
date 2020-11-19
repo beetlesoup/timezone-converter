4bfd42c7df84c551897d5d96827b0bb7229d65fc
beetlesoup <sairafi.b@gmail.com>
2020-11-19 17:49:59 +0300

    feat: Add main file ('Tell Me When.js')

---
+++ Tell Me When.js
@@ -0,0 +1,8 @@
+
+// 01: Start with the easy thing. Get the user's timezone.
+
+// The getTimezoneOffset() method returns the time difference between UTC time and local time, in minutes.
+var d = new Date();
+var utc_offset = d.getTimezoneOffset();
+
+// 02: Find out how to find all instances of a string on the page.
